c624227951fff74dec6435cb11891c15259564d9
Nick Iacullo <iacullo@usc.edu>
2023-01-05 14:48:35 -0800

    feat(ICTRAINING-1390): create a new list model
    [ICTRAINING-1409]

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from django.urls import resolve, reverse
-from lists.models import Item
+from django.urls import reverse
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -17,8 +17,9 @@ class ListViewTest(TestCase):
 
     
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        new_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=new_list)
+        Item.objects.create(text='itemey 2', list=new_list)
 
         url = '/lists/the-only-list-in-the-world/'
         response = self.client.get(url)
@@ -39,16 +40,23 @@ class NewListViewTest(TestCase):
         response = self.client.post(url, data={'item_text': 'A new list item'})
         self.assertRedirects(response, reverse('lists:view_list'))
 
-class ItemModelTest(TestCase):
-
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        first_list = List()
+        first_list.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = first_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = first_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, first_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -56,4 +64,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, first_list)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, first_list)
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from lists.models import Item
+from lists.models import Item, List
 
 def index(request):
     return render(request, 'lists/index.html')
@@ -11,5 +11,6 @@ def view_list(request):
     return render(request, 'lists/list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    new_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=new_list)
     return redirect('lists:view_list')
